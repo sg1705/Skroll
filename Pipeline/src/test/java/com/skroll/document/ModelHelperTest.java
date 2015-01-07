571f8bafd58d8aee725a4866d52a8c3445863b01
Saurabh Gupta <sg1705@gmail.com>
2015-01-07 07:01:19 -0500

    Added an annotation test to the helper class

--- Pipeline/src/test/java/com/skroll/document/ModelHelperTest.java
+++ Pipeline/src/test/java/com/skroll/document/ModelHelperTest.java
@@ -1,5 +1,6 @@
 package com.skroll.document;
 
+import com.skroll.parser.Parser;
 import com.skroll.pipeline.Pipeline;
 import com.skroll.pipeline.Pipes;
 import com.skroll.pipeline.util.Utils;
@@ -71,7 +72,44 @@ public class ModelHelperTest extends TestCase {
             e.printStackTrace();
         }
 
-
     }
+
+    @Test
+    public void testModelHelperWithAnnotations() throws Exception {
+        String fileName = "src/test/resources/document/experiment-jsoup-node-extraction.html";
+        Document doc = Parser.parseDocumentFromHtmlFile(fileName);
+
+        List<CoreMap> paragraphs = doc.getParagraphs();
+
+        // now get a Json string of the document
+        String jsonString = ModelHelper.getJson(doc);
+        Document doc2 = ModelHelper.getModel(jsonString);
+        List<CoreMap> newParas = doc2.getParagraphs();
+        System.out.println(newParas.size());
+        System.out.println(jsonString);
+
+        assert (newParas.size() == paragraphs.size());
+        assert (newParas.size() != 0);
+    }
+
+
+    @Test
+    public void testModelHelperWithAnnotationsWithSimpleDocument() throws Exception {
+        String fileName = "src/test/resources/document/simple-html-text.html";
+        Document doc = Parser.parseDocumentFromHtmlFile(fileName);
+
+        List<CoreMap> paragraphs = doc.getParagraphs();
+
+        // now get a Json string of the document
+        String jsonString = ModelHelper.getJson(doc);
+        Document doc2 = ModelHelper.getModel(jsonString);
+        List<CoreMap> newParas = doc2.getParagraphs();
+        System.out.println(newParas.size());
+        System.out.println(jsonString);
+
+        assert (newParas.size() == paragraphs.size());
+        assert (newParas.size() != 0);
+    }
+
 
 }
