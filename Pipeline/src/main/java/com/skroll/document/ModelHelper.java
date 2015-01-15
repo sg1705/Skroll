387a30cb2ece631b2de69cb876ae7fcc1fb8b0b2
Saurabh Gupta <sg1705@gmail.com>
2015-01-14 22:57:04 -0500

    Changes to JSON serializer to bring in line with the Phantom generated HTML

--- Pipeline/src/main/java/com/skroll/document/ModelHelper.java
+++ Pipeline/src/main/java/com/skroll/document/ModelHelper.java
@@ -6,6 +6,7 @@ import com.skroll.pipeline.Pipeline;
 import com.skroll.pipeline.Pipes;
 
 import java.lang.reflect.Type;
+import java.util.*;
 
 /**
  * Created by saurabh on 12/22/14.
@@ -41,10 +42,61 @@ public class ModelHelper {
         return htmlDoc;
     }
 
-    public static Gson getGson() {
-        Gson gson = new GsonBuilder().create();
+    private static Gson getGson() {
+        Gson gson = new GsonBuilder()
+                        .create();
         return gson;
     }
 
+
+    public Document fromJson(String json) throws Exception {
+
+        JsonParser parser = new JsonParser();
+        JsonElement jsonElement = parser.parse(json);
+
+        //assume that the first element is a map
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        Set<Map.Entry<String, JsonElement>> set = jsonObject.entrySet();
+        JsonElement startingMap = set.iterator().next().getValue();
+        //processJson(startingMap);
+        CoreMap coreMap = processObject(startingMap);
+        Document document = new Document(coreMap);
+        return document;
+    }
+
+     private List<CoreMap> processArray(JsonElement element) throws Exception {
+        List<CoreMap> coreMapList = new ArrayList<CoreMap>();
+        Iterator<JsonElement> elements = element.getAsJsonArray().iterator();
+        while (elements.hasNext()) {
+            JsonElement elmt = elements.next();
+            //assume that it is a list of coreMap. Hence, each element has to be a object
+            if (elmt.isJsonObject()) {
+                CoreMap map = processObject(elmt);
+                coreMapList.add(map);
+            } else {
+                System.out.println("WHOOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                throw new Exception("Deserialzation failed because array is not a List<CoreMap>");
+            }
+        }
+        return coreMapList;
+    }
+
+
+    private CoreMap processObject(JsonElement element) throws Exception {
+        CoreMap coreMap = new CoreMap();
+        Set<Map.Entry<String, JsonElement>> set = element.getAsJsonObject().entrySet();
+        for(Map.Entry<String, JsonElement> entry : set) {
+            JsonElement elmt = entry.getValue();
+            if (elmt.isJsonPrimitive()) {
+                // add in the core map
+                coreMap.set(entry.getKey(), elmt.getAsString());
+            } else if (elmt.isJsonArray()) {
+                coreMap.set(entry.getKey(), processArray(elmt));
+            } else if (elmt.isJsonObject()) {
+                coreMap.set(entry.getKey(), processObject(elmt));
+            }
+        }
+        return coreMap;
+    }
 
 }
