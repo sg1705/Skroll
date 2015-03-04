30b66721267c939f9a1bca0e625caab00012f8b2
Saurabh Gupta <sg1705@gmail.com>
2015-03-03 22:13:21 -0500

    Separting out controllers, directives and services into separate files

---
+++ Pipeline/src/main/webapp/scripts/services/documentmodel.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name SkrollApp.DocumentModel
+ * @description
+ * # DocumentModel
+ * Factory in the SkrollApp.
+ */
+
+ var documentModel = {
+         targetHtml: "",
+         isDocAvailable: false,
+         isProcessing: false,
+         fileName: "",
+         selectedParagraphId: ""
+ };
+
+
+angular.module('SkrollApp')
+    .factory('documentModel', function () {
+            return documentModel;
+    });
+
