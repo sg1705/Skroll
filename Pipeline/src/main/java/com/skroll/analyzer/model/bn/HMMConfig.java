7a93cb27ed7f18266360f1d236834d061541ae7b
wei <wei2learn@gmail.com>
2015-04-18 05:56:01 -0400

    added HMMConfig.java

---
+++ Pipeline/src/main/java/com/skroll/analyzer/model/bn/HMMConfig.java
@@ -0,0 +1,18 @@
+package com.skroll.analyzer.model.bn;
+
+/**
+ * Created by wei on 4/16/15.
+ */
+public class HMMConfig {
+    static final int DEFAULT_LENGTH = 12;
+    NBFCConfig nbfcConfig;
+    int length;
+    public HMMConfig(NBFCConfig nbfcConfig){
+        this(nbfcConfig, DEFAULT_LENGTH);
+    }
+
+    public HMMConfig(NBFCConfig nbfcConfig, int length){
+        this.nbfcConfig = nbfcConfig;
+        this.length = length;
+    }
+}
--- Pipeline/src/main/java/com/skroll/analyzer/model/bn/HMMTrainingHelper.java
+++ Pipeline/src/main/java/com/skroll/analyzer/model/bn/HMMTrainingHelper.java
@@ -4,4 +4,9 @@ package com.skroll.analyzer.model.bn;
  * Created by wei on 4/16/15.
  */
 public class HMMTrainingHelper {
+//    public static HMM createTrainingHMM(NBFCConfig config){
+//        return new HMM(
+////                NBTrainingHelper.createTrainingNBWithFeatureConditioning(config)
+//        )
+//    }
 }
