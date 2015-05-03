df1fc08076f596356abd04960b0803a947a2f70c
Saurabh Gupta <sg1705@gmail.com>
2015-05-03 10:39:05 -0400

    Added documentId to every call

--- Pipeline/src/main/webapp/scripts/app.js
+++ Pipeline/src/main/webapp/scripts/app.js
@@ -14,7 +14,8 @@ angular.module('SkrollApp', ['ngMaterial', 'ngSanitize', 'ngTouch', 'ngRoute']);
 //** when newer version of material comes out
 //this is lifted from http://goo.gl/mrWZ0F
 angular.module('SkrollApp')
-	.config(function($mdIconProvider, $routeProvider) {
+	.config(function($mdIconProvider, $routeProvider, $locationProvider) {
+	    $locationProvider.html5Mode(true);
 		$mdIconProvider
 			.iconSet('viewer', 'img/icons/sets/viewer-24.svg', 24);
 
