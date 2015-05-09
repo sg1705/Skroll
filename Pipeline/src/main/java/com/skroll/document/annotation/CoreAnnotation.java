8f12b132e0e771b433ce4ca0c88cdb0628508d73
Saurabh Gupta <sg1705@gmail.com>
2015-05-09 06:44:52 -0400

    Adde gradle tasks to start webserver

--- Pipeline/src/main/java/com/skroll/document/annotation/CoreAnnotation.java
+++ Pipeline/src/main/java/com/skroll/document/annotation/CoreAnnotation.java
@@ -1,9 +1,12 @@
 package com.skroll.document.annotation;
 
+import com.skroll.analyzer.model.RandomVariableType;
+
 /**
  * Created by saurabh on 1/3/15.
  */
 public interface CoreAnnotation<V> extends TypesafeMap.Key<V> {
 
     public Class<V> getType();
+
 }
