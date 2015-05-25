adf84fa13f791ff20d289f4de0330aab487b429c
Saurabh Gupta <sg1705@gmail.com>
2015-05-25 09:15:56 -0400

    Test for Number Tokens

--- Pipeline/src/test/java/com/skroll/analyzer/model/applicationModel/randomVariables/NumberTokensComputerTest.java
+++ Pipeline/src/test/java/com/skroll/analyzer/model/applicationModel/randomVariables/NumberTokensComputerTest.java
@@ -1,5 +1,9 @@
 package com.skroll.analyzer.model.applicationModel.randomVariables;
 
+import com.google.common.collect.Lists;
+import com.skroll.document.CoreMap;
+import com.skroll.document.Token;
+import com.skroll.document.annotation.CoreAnnotations;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,18 +11,38 @@ import static org.junit.Assert.*;
 
 public class NumberTokensComputerTest {
 
+    NumberTokensComputer nTC;
+    CoreMap m = new CoreMap();
+    Token token1;
+    Token token2;
+    Token token3;
+
     @Before
     public void setUp() throws Exception {
+        nTC = new NumberTokensComputer();
+        m = new CoreMap();
+        token1 = new Token("First");
+        token2 = new Token("token");
+        token3 = new Token("only");
+        m.set(CoreAnnotations.TokenAnnotation.class, Lists.newArrayList(token1, token2, token3));
+        m.set(CoreAnnotations.IsInTableAnnotation.class, true);
 
     }
 
     @Test
     public void testGetValue() throws Exception {
+        assert (nTC.getNumVals() == RVCreater.DEFAULT_NUM_INT_VALS);
+        int value = nTC.getValue(m);
+        assert(value == 3);
 
+        m = new CoreMap();
+        token1 = new Token("First");
+        token2 = new Token("token");
+        token3 = new Token("only");
+        m.set(CoreAnnotations.TokenAnnotation.class, Lists.newArrayList(token1, token2, token3, token1, token2, token3, token1, token2, token3, token1, token2, token3, token1, token2, token3));
+
+        value = nTC.getValue(m);
+        assert(value == 9);
     }
 
-    @Test
-    public void testGetNumVals() throws Exception {
-
-    }
 }
