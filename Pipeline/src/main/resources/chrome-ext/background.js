c1689bfa2918ac6ced5422e169da4007080be72c
Saurabh Gupta <sg1705@gmail.com>
2015-05-25 05:59:53 -0400

    Changed url of ext

--- Pipeline/src/main/resources/chrome-ext/background.js
+++ Pipeline/src/main/resources/chrome-ext/background.js
@@ -1,6 +1,6 @@
 // React when a browser action's icon is clicked.
 chrome.browserAction.onClicked.addListener(function(tab) {
    chrome.tabs.getSelected(null,function(tab) {
-        chrome.tabs.create( { url: "http://beta.skroll.io:8088/open?q=" +tab.url } );
+        chrome.tabs.create( { url: "http://k2.skroll.io:8088/open?q=" +tab.url } );
     });
 });
