8104fae7b98bff86a0369ad271d60fa64d0c56cb
Saurabh Gupta <sg1705@gmail.com>
2015-05-25 08:21:20 -0400

    Tests for FirstWords, Not InTable

---
+++ Pipeline/src/test/java/com/skroll/analyzer/model/applicationModel/randomVariables/NotInTableRVComputerTest.java
@@ -0,0 +1,46 @@
+package com.skroll.analyzer.model.applicationModel.randomVariables;
+
+import com.skroll.document.CoreMap;
+import com.skroll.document.Token;
+import com.skroll.document.annotation.CoreAnnotations;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NotInTableRVComputerTest {
+
+    NotInTableRVComputer nTC;
+    CoreMap m = new CoreMap();
+    Token token1;
+    Token token2;
+    Token token3;
+
+    @Before
+    public void setUp() throws Exception {
+        nTC = new NotInTableRVComputer();
+        m = new CoreMap();
+        token1 = new Token("First");
+        token2 = new Token("token");
+        token3 = new Token("only");
+        m.set(CoreAnnotations.IsInTableAnnotation.class, true);
+
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+        int value = nTC.getValue(m);
+        System.out.println(value);
+        assert (value == 0);
+
+        m.set(CoreAnnotations.IsInTableAnnotation.class, false);
+        value = nTC.getValue(m);
+        assert (value == 1);
+    }
+
+    @Test
+    public void testGetNumVals() throws Exception {
+        int value = nTC.getNumVals();
+        assert (value == 2);
+    }
+}
---
+++ Pipeline/src/test/java/com/skroll/analyzer/model/applicationModel/randomVariables/NumberTokensComputerTest.java
@@ -0,0 +1,24 @@
+package com.skroll.analyzer.model.applicationModel.randomVariables;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumberTokensComputerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNumVals() throws Exception {
+
+    }
+}
