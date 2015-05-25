750510ce3ae878e0317a29caaea8649785bfd640
saurabhagarwal <saurabh.wl@gmail.com>
2015-05-25 09:56:06 -0400

    RV testcases

---
+++ Pipeline/src/test/java/com/skroll/analyzer/model/applicationModel/randomVariables/RVValueSetterTest.java
@@ -0,0 +1,21 @@
+package com.skroll.analyzer.model.applicationModel.randomVariables;
+
+import org.junit.Test;
+
+public class RVValueSetterTest {
+
+    @Test
+    public void testSetValue() throws Exception {
+
+    }
+
+    @Test
+    public void testAddTerms() throws Exception {
+
+    }
+
+    @Test
+    public void testClearValue() throws Exception {
+
+    }
+}
