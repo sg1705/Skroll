ed7b15fc7054100dc8ca380dbf0df4d10aba2c06
sg1705 <sg1705@gmail.com>
2015-06-10 09:23:19 -0400

    Added timing to extractor

--- Pipeline/src/main/resources/parser/extractor/parserTest.js
+++ Pipeline/src/main/resources/parser/extractor/parserTest.js
@@ -1,10 +1,12 @@
 var DEBUG = true;
 var sourceUrl = "www.google.com";
+var startTime = Date.now();
 $(":root").contents().each(function(index, element) {
     processNode(index, element);
 });
 createLastPara();
 docObject.set(PARAGRAPH_ANNOTATION, paragraphs);
 docObject.set(TABLES_ANNOTATION, tables);
-
+var totalTime = Date.now() - startTime;
 console.log(JSON.stringify(docObject, null, 2));
+console.log("Time taken to parse:" + totalTime+ 'ms');
