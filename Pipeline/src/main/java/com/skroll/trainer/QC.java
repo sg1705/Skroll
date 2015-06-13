c55287086809d5082d28438ac773777907220e5b
saurabhagarwal <saurabh.wl@gmail.com>
2015-06-13 11:38:18 -0400

    Add BenchmarkModel code

--- Pipeline/src/main/java/com/skroll/trainer/QC.java
+++ Pipeline/src/main/java/com/skroll/trainer/QC.java
@@ -2,18 +2,19 @@ package com.skroll.trainer;
 
 import com.skroll.classifier.Category;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by saurabhagarwal on 6/6/15.
  */
 public class QC {
-    List<Stats> stats = Arrays.asList(new Stats(Category.DEFINITION),new Stats(Category.TOC_1),new Stats(Category.TOC_2),new Stats(Category.TOC_3),new Stats(Category.TOC_4),new Stats(Category.TOC_5 ));
+
+    List<Stats> stats = new ArrayList<>();
 
     public QC() {
-        if (stats.size() != Category.getCategories().size()){
-            throw new RuntimeException ("stats size should be same as number of category defined.");
+        for (int category : Category.getCategories()){
+            stats.add(new Stats(category));
         }
     }
     @Override
