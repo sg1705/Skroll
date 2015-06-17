35480e2ee91e33cf2ceb3dc4a9faea5168458456
Saurabh Gupta <sg1705@gmail.com>
2015-06-16 21:06:55 -0400

    Added BenchmarkTest and APITestGuiceModule

---
+++ Pipeline/src/test/java/com/skroll/rest/APITestGuiceModule.java
@@ -0,0 +1,35 @@
+package com.skroll.rest;
+
+import com.google.inject.AbstractModule;
+import com.skroll.classifier.ClassifierFactory;
+import com.skroll.classifier.ModelFactory;
+import com.skroll.document.factory.*;
+import com.skroll.util.Configuration;
+import com.skroll.util.TestConfiguration;
+
+/**
+ * Created by saurabh on 6/16/15.
+ */
+public class APITestGuiceModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(ModelFactory.class);
+        bind(ClassifierFactory.class);
+
+        //default binding
+        bind(DocumentFactory.class)
+                .to(CorpusFSDocumentFactoryImpl.class);
+
+
+        bind(DocumentFactory.class)
+                .annotatedWith(CorpusFSDocumentFactory.class)
+                .to(CorpusFSDocumentFactoryImpl.class);
+
+        bind(DocumentFactory.class)
+                .annotatedWith(BenchmarkFSDocumentFactory.class)
+                .to(BenchmarkFSDocumentFactoryImpl.class);
+
+        bind(Configuration.class).to(TestConfiguration.class);
+    }
+
+}
--- Pipeline/src/test/java/com/skroll/rest/benchmark/BenchmarkAPITest.java
+++ Pipeline/src/test/java/com/skroll/rest/benchmark/BenchmarkAPITest.java
@@ -5,22 +5,44 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+
 import static org.junit.Assert.*;
 
 public class BenchmarkAPITest extends APITest {
 
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
 
     @Test
     public void testSaveBenchmarkFile() throws Exception {
+        String TARGET_URL = "http://localhost:8888/restServices/doc/saveBenchmarkFile";
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(TARGET_URL);
+
+        String response = webTarget.request(MediaType.APPLICATION_JSON).cookie(new NewCookie("documentId", documentId)).get(String.class);
+        logger.debug("Here is the response: " + response);
+        assert(response.contains("benchmark file is stored"));
+        client.close();
+    }
+
+    @Test(expected = Exception.class)
+    public void testSaveBenchmarkFileWithNullDoc() throws Exception {
+        String TARGET_URL = "http://localhost:8888/restServices/doc/saveBenchmarkFile";
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(TARGET_URL);
+
+        try {
+            String response = webTarget.request(MediaType.APPLICATION_JSON).cookie(new NewCookie("documentId", "aa343")).get(String.class);
+        } catch (Exception e) {
+            logger.debug("Here is the response: " + e.getMessage());
+        } finally {
+            client.close();
+            throw new Exception();
+        }
 
     }
+
 }
