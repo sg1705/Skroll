efec30130c5d828848807c9bedab4c64767294be
Saurabh Gupta <sg1705@gmail.com>
2015-07-21 23:39:28 -0400

    Made a few services and controllers IIFE

--- Pipeline/src/main/webapp/scripts/controllers/import.js
+++ Pipeline/src/main/webapp/scripts/controllers/import.js
@@ -1,38 +1,42 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name skrollApp.controller:ImportCtrl
- * @description
- * # ImportCtrl
- * Controller of the ImportCtrl
- */
+  /**
+   * @ngdoc function
+   * @name skrollApp.controller:ImportCtrl
+   * @description
+   * # ImportCtrl
+   * Controller of the ImportCtrl
+   */
 
-var ImportCtrl = function($location, $routeParams, $http) {
-  var url = $routeParams.q;
-  var urlf = url.split('/');
-  var fileName = urlf[urlf.length - 1];
-  if (url == null) {
-    return;
+  var ImportCtrl = function($location, $routeParams, $http) {
+
+    //-- private variables
+    var url = $routeParams.q;
+    var urlf = url.split('/');
+    var fileName = urlf[urlf.length - 1];
+    if (url == null) {
+      return;
+    }
+    documentModel.isProcessing = true;
+    console.log(url);
+    $http.get('restServices/doc/importDoc?documentId=' + $routeParams.q)
+      .success(function(data) {
+        console.log(data);
+        var docId = data.documentId;
+        $location.search({});
+        $location.path('/view/docId/' + fileName);
+      });
   }
-  documentModel.isProcessing = true;
-  console.log(url);
-  $http.get('restServices/doc/importDoc?documentId=' + $routeParams.q)
-    .success(function(data) {
-      console.log(data);
-      var docId = data.documentId;
-      $location.search({});
-      $location.path('/view/docId/' + fileName);
-    });
-}
 
-angular.module('SkrollApp')
-  .controller('ImportCtrl', ['$location', '$routeParams', '$http', ImportCtrl]);
+  angular.module('SkrollApp')
+    .controller('ImportCtrl', ['$location', '$routeParams', '$http', ImportCtrl]);
 
-var BackdropCtrl = function(documentModel) {
-  this.isProcessing = documentModel.isProcessing;
-}
+  var BackdropCtrl = function(documentModel) {
+    this.isProcessing = documentModel.isProcessing;
+  }
 
-angular.module('SkrollApp')
-  .controller('BackdropCtrl', ['documentModel', BackdropCtrl]);
+  angular.module('SkrollApp')
+    .controller('BackdropCtrl', ['documentModel', BackdropCtrl]);
 
+})();
