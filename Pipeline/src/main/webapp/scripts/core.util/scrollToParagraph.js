c5137ce0aa67a6054fc59a16fc8abaa76e7241cc
Saurabh Gupta <sg1705@gmail.com>
2015-08-15 13:21:43 -0400

    removed toolbarinfo dependency

--- Pipeline/src/main/webapp/scripts/core.util/scrollToParagraph.js
+++ Pipeline/src/main/webapp/scripts/core.util/scrollToParagraph.js
@@ -35,31 +35,3 @@
   }
 
 })();
-
-// 'use strict';
-
-// /**
-//  * @ngdoc directive
-//  * @name SkrollApp.directive:fileUpload
-//  * @description
-//  * # fileUpload
-//  */
-// angular.module('SkrollApp')
-//   .directive('scrollToParagraph', [ 'documentModel', '$mdSidenav', 
-//                                     'selectionService', 'ToolbarModel', 'scrollObserverService',
-//     function(documentModel, $mdSidenav, selectionService, ToolbarModel, scrollObserverService) {
-//       return {
-//         restricted: 'A',
-//         link: function(scope, element, attrs) {
-//           var paragraphId = attrs.scrollToParagraph;
-//           var para =
-//             $(element).click(function() {
-//               scrollObserverService.notify(paragraphId);
-//               selectionService.scrollToParagraph(paragraphId);
-//               scope.$apply();
-
-//             });
-//         }
-//       }
-//     }
-//   ]);
--- Pipeline/src/main/webapp/scripts/upload/skFileUpload.js
+++ Pipeline/src/main/webapp/scripts/upload/skFileUpload.js
@@ -13,7 +13,7 @@
     .directive('skFileUpload', skFileUpload);
 
   /* @ngInject */
-  function skFileUpload(documentModel, ToolbarModel, documentService, LHSModel, $location) {
+  function skFileUpload(documentModel, documentService, LHSModel, $location) {
 
     var directive = {
       restricted: 'A',
@@ -39,7 +39,7 @@
             documentModel.fileName = data.files[0].name;
             scope.fileName = data.files[0].name;
         })
-        ToolbarModel.toolbarInfo.title = data.files[0].name;
+        //ToolbarModel.toolbarInfo.title = data.files[0].name;
         data.submit();
       }
 
@@ -76,67 +76,3 @@
   }
 
 })();
-
-// 'use strict';
-
-// /**
-//  * @ngdoc directive
-//  * @name SkrollApp.directive:fileUpload
-//  * @description
-//  * # fileUpload
-//  */
-// angular.module('SkrollApp')
-//   .directive('fileUpload', 
-//     ['documentModel', 
-//     'ToolbarModel', 
-//     'documentService',
-//     'LHSModel',
-//     '$location',
-//     function(documentModel, ToolbarModel, documentService, LHSModel, $location) {
-//       return {
-//           restricted: 'A',
-//           link: function(scope, element, attrs) {
-//            $(element).fileupload({
-//                   dataType: 'text',
-//                   add: function (e, data) {
-//                       scope.$apply(function() {
-//                           documentModel.isProcessing = true;
-//                           documentModel.fileName = data.files[0].name;
-//                           scope.fileName = data.files[0].name;
-//                       })
-//                       ToolbarModel.toolbarInfo.title = data.files[0].name;
-//                       data.submit();
-//                   },
-//                   done: function (e, data) {
-//                       var terms;
-//                       $("#content").html(data.result);
-//                       //use get terms
-//                       documentService.getTerms(documentModel.fileName).then(function(terms){
-//                         //create new LHS model items
-//                         LHSModel.setTerms(terms);
-//                         //LHSModel.smodel.terms = terms;
-//                       }, function(msg) {
-//                         console.log(msg);
-//                       });
-//                       scope.$apply(function() {
-//                           scope.targetHtml = data.result;
-//                           scope.isDocAvailable = true;
-//                           scope.isProcessing = false;
-//                           LHSModel.model = terms;
-//                           documentModel.isDocAvailable = true;
-//                           documentModel.targetHtml = data.result;
-//                           documentModel.isProcessing = false;
-//                       });
-//                       documentModel.documentId = documentModel.fileName;
-//                       $location.path('/view/docId/' + documentModel.documentId);
-//                   },
-//                   fail: function (e, data) {
-//                       console.log("failed");
-//                       console.log(e);
-//                   }
-//               });
-//           }
-//       }
-
-//   }]);
-
