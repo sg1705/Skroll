0cde6dd1da576288d09fd4bb476e5aa5b33109ac
Saurabh Gupta <sg1705@gmail.com>
2015-10-03 07:35:08 -0400

    turn off trainer by default but user trainer=true to turn it back on

--- Pipeline/src/main/webapp/scripts/core/toolbar.controller.js
+++ Pipeline/src/main/webapp/scripts/core/toolbar.controller.js
@@ -15,7 +15,7 @@
 		.controller('ToolbarCtrl', ToolbarCtrl);
 
 	/* @ngInject */
-	function ToolbarCtrl($mdSidenav, documentModel) {
+	function ToolbarCtrl($mdSidenav, documentModel, featureFlags, $location) {
 
 		//-- private variables
 		var vm = this;
@@ -26,12 +26,32 @@
 		//-- public methods
 		vm.toggleSidenav = toggleSidenav;
 
+		//-- startup action
+		enableTrainerMode($location.search());
 
 		/////////////
 
     function toggleSidenav(menuId) {
       $mdSidenav(menuId).toggle();
     };
+
+		function enableTrainerMode(queryParams) {
+      var flagName = 'trainer';
+      var flags = featureFlags.get();
+      var flag = _.find(flags, function(item){ 
+                                  if (item.key == flagName) {return true;}
+                                });
+      if (flag != null) {
+      		if (queryParams.trainer) {
+      			featureFlags.enable(flag);	
+      		} else {
+      			featureFlags.disable(flag);	
+      		}
+
+      }
+		}
+
+
 
   }
 
