8e558755f43aa74d5f48be85498b33ed0490d5df
Saurabh Gupta <sg1705@gmail.com>
2015-10-17 05:48:51 -0400

    interceptors to show error page after 10s timeout

---
+++ Pipeline/src/main/webapp/scripts/core.services/httpRequestError.interceptor.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name SkrollApp.clickObserverService
+   * @description
+   * # clickObserverService
+   * Manages all click, touch events on a paragraph
+   */
+
+  angular
+    .module('app.core.services')
+    .factory('httpRequestErrorInterceptor', HttpReqeustErrorInterceptor);
+
+  /* @ngInject */
+  function HttpReqeustErrorInterceptor($q, $location, documentModel) {
+
+    var service = {
+      responseError : responseError,
+      request : request
+    };
+
+    return service;
+
+
+    //-- private methods
+
+    /**
+     * Method to handle error
+     **/
+    function responseError(rejection) {
+      // do something on error
+      // if ((rejection.status === 500) || (rejection === 404) || (rejection === 0)){
+          documentModel.isProcessing = false;
+          $location.path('/error/');
+      // }
+      return $q.reject(rejection);
+    };
+
+
+    function request(config) {
+      config.timeout = 10000;
+      return config;
+    }
+
+  }
+
+})();
