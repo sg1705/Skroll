a11aadb76eb15b2d44d7712136a60bd1db2eebd8
wei2learn <wei2learn@gmail.com>
2015-11-19 20:18:41 -0500

    removed some files from git. and added conversion from persisted docs to text file.

--- Pipeline/src/main/java/com/skroll/document/ConvertDocToText.java
+++ Pipeline/src/main/java/com/skroll/document/ConvertDocToText.java
@@ -22,6 +22,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by wei on 11/17/15.
@@ -33,6 +35,9 @@ public class ConvertDocToText {
 
     public static DocumentFactory corpusDocumentFactory;
     static final String INPUT_FOLDER = "build/resources/main/preEvaluated/";
+//    static final String OUTPUT_FOLDER = "build/resources/main/preEvaluatedTxt/";
+    static final String OUTPUT_FILE_NAME = "build/resources/main/preEvaluatedTxtOneFile.txt";
+
     public static void convertDocsToText () throws Exception {
 
         try {
@@ -49,27 +54,28 @@ public class ConvertDocToText {
             e.printStackTrace();
         }
 
-        FluentIterable<File> iterable = Files.fileTreeTraverser().breadthFirstTraversal(new File(INPUT_FOLDER));
-        for (File f : iterable) {
-            if (f.isFile()) {
-                Document doc = corpusDocumentFactory.get(f.getName());
-                //iterate over each paragraph
-                if (doc == null) {
-                    logger.error("Document can't be parsed. failed to train the model");
-                    return;
-                }
+        try ( PrintWriter writer = new PrintWriter(OUTPUT_FILE_NAME, "UTF-8")) {
+            FluentIterable<File> iterable = Files.fileTreeTraverser().breadthFirstTraversal(new File(INPUT_FOLDER));
+            for (File f : iterable) {
+                if (f.isFile()) {
+                    Document doc = corpusDocumentFactory.get(f.getName());
+                    //iterate over each paragraph
+                    if (doc == null) {
+                        logger.error("Document can't be parsed. failed to conver doc " + f.getName());
+                        return;
+                    }
 
 
+                    //                try ( PrintWriter writer = new PrintWriter(OUTPUT_FOLDER + f.getName() + ".txt" , "UTF-8")) {
+                    for (CoreMap paragraph : doc.getParagraphs()) {
+                        writer.println(paragraph.getText());
+                    }
 
 
-                for (CoreMap paragraph : doc.getParagraphs()) {
-                    System.out.println(paragraph.getText());
-                }
-                try {
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
             }
+        } catch (IOException ex){
+                    ex.printStackTrace(System.out);
         }
     }
 
