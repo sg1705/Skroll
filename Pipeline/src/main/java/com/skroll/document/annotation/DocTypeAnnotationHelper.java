e564396e6c586a7d6a67e7e50782e6144ce5edc4
saurabhagarwal <saurabh.wl@gmail.com>
2015-12-18 00:23:46 -0500

    To resolve the duplicate training of same items (doctype and Toc), added check for preWeight. Added a benchmark file for test

--- Pipeline/src/main/java/com/skroll/document/annotation/DocTypeAnnotationHelper.java
+++ Pipeline/src/main/java/com/skroll/document/annotation/DocTypeAnnotationHelper.java
@@ -26,28 +26,33 @@ public class DocTypeAnnotationHelper {
 
     /**
      * For a document, from the document level annotation, get the docType.
+     *
      * @param document
      * @return List of List of Token String
      */
     public static int getDocType(CoreMap document) {
         HashMap<Integer, CoreMap> categoryAnnotation = document.get(CoreAnnotations.CategoryAnnotations.class);
-        if (categoryAnnotation==null)
+        if (categoryAnnotation == null)
             return Category.DOCTYPE_NONE;
         if (categoryAnnotation.keySet().iterator().hasNext()) {
             return categoryAnnotation.keySet().iterator().next();
         }
         return Category.DOCTYPE_NONE;
     }
+
     /**
      * For a document, from the document level annotation, get the docType.
+     *
      * @param document
      * @return List of List of Token String
      */
-    public static HashMap<Integer, CoreMap>  getDocLevelCategoryAnnotation(CoreMap document) {
-         return document.get(CoreAnnotations.CategoryAnnotations.class);
+    public static HashMap<Integer, CoreMap> getDocLevelCategoryAnnotation(CoreMap document) {
+        return document.get(CoreAnnotations.CategoryAnnotations.class);
     }
+
     /**
      * For a document, from the document level annotation, get the docType.
+     *
      * @param document
      * @return List of List of Token String
      */
@@ -57,17 +62,18 @@ public class DocTypeAnnotationHelper {
 
     /**
      * For a document, from the document level annotation, get the docType.
+     *
      * @param document
      * @return List of List of Token String
      */
     public static void copyParaLevelCategoryAnnotationToDocLevel(CoreMap document, CoreMap paragraph) {
         document.set(CoreAnnotations.CategoryAnnotations.class, paragraph.get(CoreAnnotations.CategoryAnnotations.class));
         HashMap<Integer, CoreMap> categoryAnnotation = document.get(CoreAnnotations.CategoryAnnotations.class);
-        if (categoryAnnotation!=null && categoryAnnotation.keySet().iterator().hasNext()) {
+        if (categoryAnnotation != null && categoryAnnotation.keySet().iterator().hasNext()) {
             int doctype = categoryAnnotation.keySet().iterator().next();
             CoreMap annotationCoreMap = categoryAnnotation.get(doctype);
-            if (annotationCoreMap!=null){
-                if(annotationCoreMap.size()!=0) {
+            if (annotationCoreMap != null) {
+                if (annotationCoreMap.size() != 0) {
                     annotationCoreMap.set(CoreAnnotations.TermTokensAnnotation.class, null);
                 }
             }
@@ -76,33 +82,34 @@ public class DocTypeAnnotationHelper {
 
     /**
      * Annotated a given Paragraph with given List of token and a given categoryId
+     *
      * @param document
      * @param docType
      */
-    public static void annotateDocTypeWithWeightAndUserObservation(Document document,int docType, float currentCategoryWeight) {
+    public static void annotateDocTypeWithWeightAndUserObservation(Document document, int docType, float currentCategoryWeight) {
         HashMap<Integer, CoreMap> categoryAnnotation = new HashMap<>();
-            CoreMap annotationCoreMap = new CoreMap();
-            annotationCoreMap.set(CoreAnnotations.CurrentCategoryWeightFloat.class, currentCategoryWeight);
-            annotationCoreMap.set(CoreAnnotations.PriorCategoryWeightFloat.class, 0f);
-            categoryAnnotation.put(docType, annotationCoreMap);
-            document.set(CoreAnnotations.CategoryAnnotations.class, categoryAnnotation);
-            document.set(CoreAnnotations.IsUserObservationAnnotation.class, true);
+        CoreMap annotationCoreMap = new CoreMap();
+        annotationCoreMap.set(CoreAnnotations.CurrentCategoryWeightFloat.class, currentCategoryWeight);
+        annotationCoreMap.set(CoreAnnotations.PriorCategoryWeightFloat.class, 0f);
+        categoryAnnotation.put(docType, annotationCoreMap);
+        document.set(CoreAnnotations.CategoryAnnotations.class, categoryAnnotation);
+        document.set(CoreAnnotations.IsUserObservationAnnotation.class, true);
     }
 
     /**
      * Annotated a given Paragraph with given List of token and a given categoryId
+     *
      * @param doc
      * @param docType
      */
-    public static void annotateDocType(Document doc,int docType) {
+    public static void annotateDocType(Document doc, int docType) {
         HashMap<Integer, CoreMap> categoryAnnotation = new HashMap<>();
-        CoreMap annotationCoreMap  = new CoreMap();
-        categoryAnnotation.put(docType,annotationCoreMap);
+        CoreMap annotationCoreMap = new CoreMap();
+        categoryAnnotation.put(docType, annotationCoreMap);
         doc.set(CoreAnnotations.CategoryAnnotations.class, categoryAnnotation);
     }
 
-    public static void trainDocType (List <Classifier> classifiersForDocType, Document document) throws Exception
-    {
+    public static void trainDocType(List<Classifier> classifiersForDocType, Document document) throws Exception {
         Document singleParaDoc = getEntireDocCollaspedAsSingleParagraph(document);
         //iterate over each paragraph
         if (singleParaDoc == null) {
@@ -122,10 +129,10 @@ public class DocTypeAnnotationHelper {
                 CategoryAnnotationHelper.copyCurrentCategoryWeightsToPriorForDocType(paragraph);
             }
         }
-        copyParaLevelCategoryAnnotationToDocLevel(document,singleParaDoc.getParagraphs().get(0));
+        copyParaLevelCategoryAnnotationToDocLevel(document, singleParaDoc.getParagraphs().get(0));
     }
 
-    public static void classifyDocType (List<Classifier> classifiersForDocType, Document document) throws Exception {
+    public static void classifyDocType(List<Classifier> classifiersForDocType, Document document) throws Exception {
 
         Document singleParaDoc = getEntireDocCollaspedAsSingleParagraph(document);
         try {
@@ -135,34 +142,47 @@ public class DocTypeAnnotationHelper {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        copyParaLevelCategoryAnnotationToDocLevel(document,singleParaDoc.getParagraphs().get(0));
+        copyParaLevelCategoryAnnotationToDocLevel(document, singleParaDoc.getParagraphs().get(0));
         //int docType = DocTypeAnnotationHelper.extractDocTypeFromSingleParaDocument(singleParaDoc.getParagraphs().get(0));
         //DocTypeAnnotationHelper.annotateDocType(document,docType);
     }
 
-    public static Document getEntireDocCollaspedAsSingleParagraph(Document entireDocument) throws Exception{
+    public static Document getEntireDocCollaspedAsSingleParagraph(Document entireDocument) throws Exception {
 
         Document singleParaDocument = new Document();
         CoreMap singleParagraph = new CoreMap("collapsedPara", "collapsedPara");
 
         List<Token> allTokens = entireDocument.getParagraphs()
                 .stream()
-                .flatMap( paragraph -> paragraph.get(CoreAnnotations.TokenAnnotation.class).stream())
+                .flatMap(paragraph -> paragraph.get(CoreAnnotations.TokenAnnotation.class).stream())
                 .collect(Collectors.toList());
         singleParagraph.set(CoreAnnotations.TokenAnnotation.class, allTokens);
-        DocTypeAnnotationHelper.copyDocLevelCategoryAnnotationToParaLevel(entireDocument,singleParagraph);
+        DocTypeAnnotationHelper.copyDocLevelCategoryAnnotationToParaLevel(entireDocument, singleParagraph);
         List<CoreMap> paraList = new ArrayList<>();
         paraList.add(singleParagraph);
-       /*
-        int docType = DocTypeAnnotationHelper.getDocType(entireDocument);
-        float currentWeight = DocTypeAnnotationHelper.getDocTypeTrainingWeight(entireDocument);
-        CategoryAnnotationHelper.annotateCategoryWeight(singleParagraph, docType,currentWeight);
-        */
+
         boolean isUserObserved = entireDocument.containsKey(CoreAnnotations.IsUserObservationAnnotation.class);
         if (isUserObserved == true) {
             singleParagraph.set(CoreAnnotations.IsUserObservationAnnotation.class, isUserObserved);
         }
         singleParaDocument.set(CoreAnnotations.ParagraphsAnnotation.class, paraList);
         return singleParaDocument;
+    }
+
+    /**
+     * Clear Prior training weight of all categories for a paragraph
+     *
+     * @param document
+     */
+    public static void clearPriorCategoryWeight(Document document) {
+        HashMap<Integer, CoreMap> categoryAnnotation = document.get(CoreAnnotations.CategoryAnnotations.class);
+        if (categoryAnnotation == null) return;
+        for (int categoryId : Category.getDocType()) {
+            CoreMap annotationCoreMap = categoryAnnotation.get(categoryId);
+            if (annotationCoreMap != null) {
+                annotationCoreMap.set(CoreAnnotations.PriorCategoryWeightFloat.class, 0f);
+                logger.debug("Cleared \t {} \t {}", document.getId(), categoryId);
+            }
+        }
     }
 }
