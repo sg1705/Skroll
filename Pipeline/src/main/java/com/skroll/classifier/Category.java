ea424f0ebad1ea0bbf026392d88e114af5516159
saurabhagarwal <saurabh.wl@gmail.com>
2015-12-20 12:00:42 -0500

    To resolve the duplicate training of same items (doctype and Toc), added check for preWeight. Added a benchmark file for test

--- Pipeline/src/main/java/com/skroll/classifier/Category.java
+++ Pipeline/src/main/java/com/skroll/classifier/Category.java
@@ -1,7 +1,11 @@
 package com.skroll.classifier;
 
+import com.google.common.collect.Maps;
+
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by saurabhagarwal on 1/5/15.
@@ -20,8 +24,24 @@ public class Category {
     public static final int TEN_Q = 102;
     public static final int INDENTURE = 103;
     public static final int S4 = 104;
+
+    private static final Map<String,Integer> docTypeNameToIdMap = new HashMap<String, Integer>()
+    {{
+            put("10-K", 101);
+            put("10-Q", 102);
+            put("Indenture", 103);
+            put("S4", 103);
+        }};
+
     int id;
     String name;
+
+    public static Integer getIDFromDocName(String DocTypeName) {
+        if(docTypeNameToIdMap.get(DocTypeName) == null){
+            return DOCTYPE_NONE;
+        }
+        return docTypeNameToIdMap.get(DocTypeName);
+    }
 
     public static List<Integer> getCategories()  {
         return Arrays.asList(Category.NONE, Category.DEFINITION,Category.TOC_1,Category.TOC_2, Category.USER_TOC); //Category.TOC_3,Category.TOC_4,Category.TOC_5 );
