e6aae8fe8d24d347fa2aa2b635598dc53916fca5
Wei Zhou <wei2learn@gmail.com>
2015-12-20 21:39:32 -0500

    added a flag for keeping existing annotation. Used by sec model to keep existing level 2 annotation put by doc model.

--- Pipeline/src/main/java/com/skroll/analyzer/model/applicationModel/ProbabilityDocumentTOCAnnotatingModel.java
+++ Pipeline/src/main/java/com/skroll/analyzer/model/applicationModel/ProbabilityDocumentTOCAnnotatingModel.java
@@ -146,6 +146,7 @@ public class ProbabilityDocumentTOCAnnotatingModel extends ProbabilityTextAnnota
             secModel.setNumIterations(SEC_NUM_ITERATION);
             secModel.setEnforcingDominatingFeatureForClass(1); // class index 1 represent level 2 in the section model.
             secModel.setUseFirstParaFormat(true);
+            secModel.setKeepExistingAnnotation(true);
 
             secModel.initialize();
             secModel.annotateParagraphs();
