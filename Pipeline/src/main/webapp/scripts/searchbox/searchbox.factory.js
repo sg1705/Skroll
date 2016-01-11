e5cfcef5234a19af741c3d67e0c55eec3405fe17
sg1705@gmail.com <sg1705@gmail.com>
2016-01-10 18:09:27 -0800

    integrated autocomplete with secSearch

--- Pipeline/src/main/webapp/scripts/searchbox/searchbox.factory.js
+++ Pipeline/src/main/webapp/scripts/searchbox/searchbox.factory.js
@@ -39,13 +39,22 @@
     //////////////
 
     /**
-     * Clear search
+     * Return query string text
      **/
     function getText() {
       var wholeSearchText = service.selectedChips.map(function(elem) { return elem.name}).join(" ") +service.searchState.searchText;
       console.log( "wholeSearchText:" + wholeSearchText);
       return wholeSearchText;
     };
+
+    /**
+     * Set query string text
+     **/
+    function setText(text) {
+      this.searchText = text;
+    };
+
+
     /**
      * Clear search
      **/
